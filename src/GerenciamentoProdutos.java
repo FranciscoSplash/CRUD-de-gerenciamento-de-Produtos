22ba8f5180081e93781b2cf6ee6415b2e0f900c1
Francisco Mauricio <franciscomauricio796@gmail.com>
2026-04-02 19:55:59 -0300

    construcao dos metodos principais

--- src/GerenciamentoProdutos.java
+++ src/GerenciamentoProdutos.java
@@ -7,19 +7,39 @@ public class GerenciamentoProdutos {
 
 
     public void CadastrarProdutos(String nome){
+        System.out.println("Digete o produto");
         produtos.add(nome);
         System.out.println("Produto cadastrado com sucesso");
     }
 
     public void listarProdutos(){
+        System.out.println("========Lista de produtos cadastrados====");
         if(produtos.isEmpty()){
             System.out.println("Sem produtos cadastrados");
         }
-        else {
-            System.out.println(produtos);
+        for(int i=0; i<produtos.size(); i++){
+            System.out.println(i +1 +" - "+ produtos.get(i));
         }
-    }
-    public void atualizarProdutos(String atualizarNome){
 
+
+    }
+    public void atualizarProdutos(String atualizarNome, int i){
+        System.out.println("Digete o produtos que pretendes atualizar");
+        if(i<0 || i>= produtos.size()){
+            System.out.println("Indice invalido");
+        }
+
+        String listar = produtos.get(i);
+        produtos.set(i, atualizarNome);
+        System.out.printf(listar, atualizarNome);
+    }
+
+
+    public void excluirProdutos(String nome){
+        System.out.println("Digete o nome que pretendes apagar");
+        if(produtos.contains(nome)){
+        produtos.remove(nome);
+            System.out.println("Nome apagado com sucesso");
+        }
     }
 }
